19421d02996a9fc29bf2911315d3829e1ee92dc1
Eduardo Farias <heydufarias@gmail.com>
2026-09-24 17:28:46 -0300

    refactor: Update Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
-import { MotionFade } from "./motion/MotionFade";
 import { cn } from "../lib/utils";
+import { MotionFade } from "./motion/MotionFade";
 
 interface HeaderProps {
   showInfoButton: boolean;
@@ -14,7 +14,7 @@ export function Header({ showInfoButton, onInfoButtonClick }: HeaderProps) {
     <header className="flex items-center justify-between px-4 sm:px-6 py-2">
       <div className={cn(
         "flex flex-col",
-        "text-[26px] sm:text-[34px] font-semibold tracking-tight leading-none"
+        "text-[27px] sm:text-[34px] tracking-tight leading-none"
       )}>
         <span>Breathe,</span>
         <span className="-mt-1.5">it is free.</span>
@@ -23,10 +23,10 @@ export function Header({ showInfoButton, onInfoButtonClick }: HeaderProps) {
         <button
           onClick={onInfoButtonClick}
           className={cn(
-            "text-[20px] sm:text-[25px] font-semibold",
+            "text-[20px] sm:text-[25px]",
             "cursor-pointer"
           )}>
-          {t("info.howToUse.title")}
+          {t("about")}
         </button>
       </MotionFade>
     </header>
